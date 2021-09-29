288d6418a74a34529e95f037cce2439c7afe5be1
hicham68 <hichams68@hotmail.fr>
2021-09-29 10:37:50 +0200

    update 11

--- index2.php
+++ index2.php
@@ -20,7 +20,7 @@ $equipes = json_decode($json); // decode un fichier string JSON en tableau d'obj
     </header> 
     <?php
     foreach ($equipes as $equipe) {   
-        // mise a jours 10?>
+        // mise a jours 11git ad?>
             <div class="teamName">
             <h2> <?php echo $equipe->nom; ?> :</h2> 
                 <img src="<?php echo $equipe->logo; ?>"/>
