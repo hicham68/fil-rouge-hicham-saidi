3882956b209dcc3a186d34a5d5d2e640b08d189b
hicham68 <71725850+hicham68@users.noreply.github.com>
2021-10-21 00:45:33 +0200

    Create coach.php

---
+++ api/coach.php
@@ -0,0 +1,140 @@
+<?php
+    class Employee{
+
+    
+       
+        // Table
+        private $db_table = "coach_equipe";
+
+        // Columns
+        public $id;
+        public $nom;
+        public $age;
+        public $equipe;
+                
+        
+        // GET ALL
+        public function getCoach(){
+            $connexion= DataBase::connect_db();
+            $sqlQuery = "SELECT id, nom, age, equipe FROM " . $this->db_table . "";
+            $stmt = $connexion->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        // CREATE
+        public function createCoach(){
+            $sqlQuery = "INSERT INTO
+                        ". $this->db_table ."
+                    SET
+                        name = :name, 
+                        email = :email, 
+                        age = :age, 
+                        designation = :designation, 
+                        created = :created";
+        
+            $stmt = $this->connexion->prepare($sqlQuery);
+        
+            // sanitize
+            $this->name=htmlspecialchars(strip_tags($this->name));
+            $this->email=htmlspecialchars(strip_tags($this->email));
+            $this->age=htmlspecialchars(strip_tags($this->age));
+            $this->designation=htmlspecialchars(strip_tags($this->designation));
+            $this->created=htmlspecialchars(strip_tags($this->created));
+        
+            // bind data
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":age", $this->age);
+            $stmt->bindParam(":designation", $this->designation);
+            $stmt->bindParam(":created", $this->created);
+        
+            if($stmt->execute()){
+               return true;
+            }
+            return false;
+        }
+
+        // READ single
+        public function getSingleEmployee(){
+            $sqlQuery = "SELECT
+                        id, 
+                        name, 
+                        email, 
+                        age, 
+                        designation, 
+                        created
+                      FROM
+                        ". $this->db_table ."
+                    WHERE 
+                       id = ?
+                    LIMIT 0,1";
+
+            $stmt = $this->connexion->prepare($sqlQuery);
+
+            $stmt->bindParam(1, $this->id);
+
+            $stmt->execute();
+
+            $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->name = $dataRow['name'];
+            $this->email = $dataRow['email'];
+            $this->age = $dataRow['age'];
+            $this->designation = $dataRow['designation'];
+            $this->created = $dataRow['created'];
+        }        
+
+        // UPDATE
+        public function updateEmployee(){
+            $sqlQuery = "UPDATE
+                        ". $this->db_table ."
+                    SET
+                        name = :name, 
+                        email = :email, 
+                        age = :age, 
+                        designation = :designation, 
+                        created = :created
+                    WHERE 
+                        id = :id";
+        
+            $stmt = $this->connexion->prepare($sqlQuery);
+        
+            $this->name=htmlspecialchars(strip_tags($this->name));
+            $this->email=htmlspecialchars(strip_tags($this->email));
+            $this->age=htmlspecialchars(strip_tags($this->age));
+            $this->designation=htmlspecialchars(strip_tags($this->designation));
+            $this->created=htmlspecialchars(strip_tags($this->created));
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            // bind data
+            $stmt->bindParam(":name", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":age", $this->age);
+            $stmt->bindParam(":designation", $this->designation);
+            $stmt->bindParam(":created", $this->created);
+            $stmt->bindParam(":id", $this->id);
+        
+            if($stmt->execute()){
+               return true;
+            }
+            return false;
+        }
+
+        // DELETE
+        function deleteEmployee(){
+            $sqlQuery = "DELETE FROM " . $this->db_table . " WHERE id = ?";
+            $stmt = $this->connexion->prepare($sqlQuery);
+        
+            $this->id=htmlspecialchars(strip_tags($this->id));
+        
+            $stmt->bindParam(1, $this->id);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
+    }
+?>
